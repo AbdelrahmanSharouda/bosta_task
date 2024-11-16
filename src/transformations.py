d7837203bd70e289c8775e52c612fe5ec6c4f9a0
Abdelrahman Shawki <abdelrahman.shawki98@gmail.com>
2024-11-16 13:28:28 +0200

    Built flatten function:

--- src/transformations.py
+++ src/transformations.py
@@ -1,1 +1,17 @@
 import pandas as pd
+
+def flatten(doc, parent_key='', sep='_'):
+    items = []
+    for k,v in doc.items():
+        new_key = f'{parent_key}{sep}{k}' if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten(v, new_key, sep=sep).items())
+        elif isinstance(v, list):
+            for i, sub_item in enumerate(v):
+                if isinstance(sub_item,dict):
+                    items.extend(flatten(sub_item, f'{new_key}{sep}{i}', sep=sep).items())
+                else:
+                    items.append(f"{new_key}{sep}{i}", sub_item)
+        else:
+            items.append(new_key,v)
+    return (items)
